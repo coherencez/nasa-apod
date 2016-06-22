881178622ab0e031968c9797c0586e2b68c57f64
Thomas Swann <tkswann@mac.com>
2016-06-22 09:17:08 -0500

    random images button working

--- js/app.js
+++ js/app.js
@@ -1,2 +1,33 @@
 'use strict';
-console.log($('<p>helllo</p>'));
+
+
+
+$('<button/>').text('Get Random Date').on('click', function () {
+
+	var rndYear = Math.floor(Math.random() * (2017 - 1995)) + 1995;
+	var rndMonth = Math.floor(Math.random() * (13 - 1)) + 1;
+	var rndDay = Math.floor(Math.random() * (32 - 1)) + 1;
+	
+	var year = rndYear.toString();
+	var month = rndMonth.toString();
+	var day = rndDay.toString();
+	
+	if (month.length === 1) {
+		month = ('0' + month);
+	}
+	
+	if (day.length === 1) {
+		day = ('0' + day);
+	}
+
+	$.ajax({
+		url: `https://api.nasa.gov/planetary/apod?date=${year}-${month}-${day}&api_key=BlNiKpyUAovsd7JgTbofzaqUkFrYoFwpAI63SE8x`
+	}).done(function (data) { 
+			let apodInfo = data;
+			$('<img/>').attr('src', apodInfo.url).appendTo($('body'));
+			console.log("data", apodInfo);
+	});
+
+
+}).appendTo($('body'));
+
